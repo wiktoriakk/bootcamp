f812c9b29b12efe7b823f81a70e23b7be15d109c
wiktoriakk <kosek.wiktoria1@gmail.com>
2024-06-24 18:38:15 +0200

    first commit

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    <br />
+    <br />
+    <form action="#" @submit="handleSubmit">
+      <label for="name">Enter your name: &nbsp;</label>
+      <input id="name" alt="Name" type="text" />
+      <button type="submit">Click Me!</button>
+    </form>
+    <section id="greeting">{{ greeting }}</section>
+  </main>
+</template>

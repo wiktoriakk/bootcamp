a61ad64ea89792c49720512a519da441131f7e9d
wiktoriakk <kosek.wiktoria1@gmail.com>
2024-06-24 19:09:02 +0200

    Added counter

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
+    Siema!
+  <br/>
+    {{ licznik }}
   </main>
 </template>
+
+</script>
+export default {
+  data() {
+    return {
+      licznik: 0
+    }
+  },
+}
+</script>
